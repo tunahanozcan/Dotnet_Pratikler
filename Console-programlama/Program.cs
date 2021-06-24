85d432fbabc05e3f25731f862a592bfe10aff132
Tunahan ÖZCAN <77634378+tunahanozcan@users.noreply.github.com>
2021-06-24 09:29:21 +0300

    Add files via upload

---
+++ Console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arabanızın markasını girin");
+            string marka=Console.ReadLine();
+            Console.WriteLine("Arabanızın modelini girin");
+            string model=Console.ReadLine();
+            Console.WriteLine($"{marka} marka arabanın {model} modelini kullanıyorsun."  );
+            Console.ReadLine();
+        }
+    }
+}
