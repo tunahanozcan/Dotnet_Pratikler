760feb0acd105aa9d7c2a47c667c2000194fb550
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-28 17:41:56 +0300

    if-else

---
+++ if-else-if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=DateTime.Now.Hour;
+            string sonuc;
+
+            if(time>=6&&time<11)
+                Console.WriteLine("Gunaydin saat:" +time);
+
+            if(time<18)
+                Console.WriteLine("Iyi gunler saat:" +time);
+            
+            else
+                Console.WriteLine("İyi Aksamlar saat:" +time);
+
+            sonuc= time>=6&&time<11 ? "Gunaydin" : time<18 ? "iyi Gunler" : "iyi geceler"; 
+            
+            Console.WriteLine(sonuc);
+        }
+    }
+}
