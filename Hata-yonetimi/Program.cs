4c538634d0f37900a77b4d75361c0be3c0be28e6
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-28 17:19:15 +0300

    hata-yonetimi

---
+++ Hata-yonetimi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("bir sayi giriniz: ");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gridiginiz sayi: "+sayi);
+                
+            }
+            catch(ArgumentNullException ex)
+            {
+                
+                Console.WriteLine("Bos bir deger girdiniz!!:"+ex.Message.ToString());
+            }
+
+            catch(FormatException ex)
+            {
+                
+                Console.WriteLine("Girdiginiz deger sayi degil!!:"+ex.Message.ToString());
+            }
+
+            catch(OverflowException ex)
+            {
+                
+                Console.WriteLine("Fazla ya da az sayıda girdiniz var!!:"+ex.Message.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("--İslem tamam--");
+            }
+        }
+    }
+}
