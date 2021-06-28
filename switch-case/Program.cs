85d367da355ecba28db41b55f12760d7c2463887
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-28 18:06:21 +0300

    switch-case

---
+++ switch-case/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month =DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 12 :
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Mevsimi.");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimi.");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimi.");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Mevsimi.");
+                    break;
+                default:
+                    Console.WriteLine("Miladi takvimde değilsin.");
+                    break;
+            }
+        }
+    }
+}
