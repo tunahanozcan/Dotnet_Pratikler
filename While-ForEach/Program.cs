76f20f32240aecf2aeac4995aea6f52ed38009fd
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-29 16:47:51 +0300

    While-forEach

---
+++ While-ForEach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace While_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Sayı giriniz.");
+            // int sayi=int.Parse(Console.ReadLine());
+            // int toplam=0;
+            // int sayac=1;
+            // while (sayac<=sayi)
+            // {
+            //     toplam+=sayac;
+            //     sayac++;
+            // }
+            // Console.WriteLine(toplam/sayi);
+
+            // char character='a';
+            // while (character <='z')
+            // {
+            //     Console.WriteLine(character);
+            //     character++;
+            // }
+
+            string[] arabalar={"Ford","Range-Rover","Audi","BMW"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
