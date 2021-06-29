871ca79c60bc9369b05f8b8150fa96fcec803ce7
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-29 16:22:13 +0300

    For-Loop

---
+++ Donguler-For-Loop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Donguler_For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("Bir sayı giriniz: ");
+            // int sayac=int.Parse(Console.ReadLine());
+            // for (int i = 0; i <= sayac; i++)
+            // {
+            //     if(i%2==1)
+            //         Console.WriteLine(i);
+            // }
+
+            // int tekToplam=0;
+            // int ciftToplam=0;
+            // for (int i = 0; i <= 100; i++)
+            // {
+            //     if(i%2==1)
+            //         tekToplam+=i;
+            //     else
+            //         ciftToplam+=i;
+            // }
+            // Console.WriteLine("100'e kadar olan çift sayıların toplamı: "+ ciftToplam);
+            // Console.WriteLine("100'e kadar olan tek sayıların toplamı: "+ tekToplam);
+
+            for (int i = 0; i < 10; i++)
+            {
+                if(i==4)
+                    continue;
+                else if(i==8)
+                    break;
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
