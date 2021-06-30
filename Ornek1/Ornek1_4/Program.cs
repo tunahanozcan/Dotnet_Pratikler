4b5d4b903b22f016d13bf2ae7940e44ddc040a0c
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-30 12:28:55 +0300

    Ornek1_4

---
+++ Ornek1/Ornek1_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ornek1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+        Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+        Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+        */
+            Console.Write("Cümleyi girin: ");
+            string cumle=Console.ReadLine();
+            string[] kelimeler=cumle.Split(" ");
+            var kelimeSayisi=0;
+
+            foreach (var kelime in kelimeler)
+            {
+            if (kelime != "")
+                kelimeSayisi++;
+            }
+            var harfSayisi=cumle.Replace(" ", string.Empty).Length;
+            Console.WriteLine("Kelime sayısı: "+kelimeSayisi);
+            Console.WriteLine("Harf sayısı: "+harfSayisi);
+            
+        }
+    }
+}
