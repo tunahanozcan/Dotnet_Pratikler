bf2dbbad5265b23342630775011ec1b0ffd282ee
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-30 09:24:20 +0300

    diziler-array

---
+++ Diziler-Array-Sinif-Method/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Diziler_Array_Sinif_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] sayiDizisi={23,12,4,86,72,3,11,17};
+
+            //Sort
+           Console.WriteLine("**** Sırasız Dizi *****");
+           foreach (var sayi in sayiDizisi)
+               Console.WriteLine(sayi);
+            
+            Console.WriteLine("**** Sıralı Dizi ****");
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+                            
+            //Clear
+            Console.WriteLine("**** Array Clear ****");
+            //sayiDizisi elemanlarını kullanarak 2. indexten itibaren 2 tane eleman 0'lar.
+            Array.Clear(sayiDizisi,2,2);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+                
+            //Reverse
+            Console.WriteLine("**** Array Reverse ****");
+            Array.Reverse(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            //IndexOf
+            Console.WriteLine("**** Array IndexOf ****");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize
+            Console.WriteLine("**** Array Resize ****");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=99;
+
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+                
+        }
+    }
+}
