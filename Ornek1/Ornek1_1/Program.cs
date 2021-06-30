6de23b05e9f72f44ea07c32ad602f41d8cb3bf43
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-06-30 11:33:01 +0300

    Ornek1_1

---
+++ Ornek1/Ornek1_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ornek1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+            Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+            Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            */
+            Console.Write("Kaç adet sayı gireceksiniz: ");
+            int sayiAdet=int.Parse(Console.ReadLine());
+            int sayac=0;
+            int[] cift=new int[sayiAdet];
+            for (int i = 0; i < sayiAdet; i++)
+            {
+                Console.Write("{0}. sayıyı girin: ",i+1);
+                int sayi =int.Parse(Console.ReadLine());
+
+                if(sayi%2==0)
+                    {
+                        cift[sayac]=sayi;
+                        sayac++;
+                    }
+            }
+            Console.WriteLine("Çift olanlar;");
+            for (int i = 0; i < sayac; i++)
+            {
+                Console.WriteLine(cift[i]);
+            }
+        }
+    }
+}
