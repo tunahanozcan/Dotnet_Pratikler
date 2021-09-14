6ad0a37971ee7ca72299d28df18d5ee53b608beb
Tunahan Özcan <tunahan.ozcan@tesan.com.tr>
2021-09-14 20:13:44 +0300

    Örnek3 eklendi.

---
+++ Ornek2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*Soru - 3:
+         Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.*/
+         Console.Write("Cümle giriniz: ");
+         List<char> harfler=new List<char>();
+         var girilen=Console.ReadLine();
+         foreach (var harf in girilen)
+         {
+             if(harf=='a'||harf=='A'||harf=='e'||harf=='E'||harf=='ı'||harf=='I'||harf=='i'||harf=='İ'||harf=='o'||harf=='O'||harf=='ö'||harf=='Ö'||harf=='u'||harf=='U'||harf=='ü'||harf=='Ü')
+                harfler.Add(harf);
+         }
+            harfler.ForEach(item=>Console.Write(item+" "));
+        }
+    }
+}
